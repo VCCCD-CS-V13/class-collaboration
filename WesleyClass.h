f91c3a923395bc2794fb5be098b8de203b7e5b28
VC Science Labs <vscilab@vcccd.edu>
2018-02-12 14:39:49 -0800

    added my .h file

---
+++ WesleyClass.h
@@ -0,0 +1,15 @@
+#pragma once
+class WesleyClass
+{
+private:
+	float firstNum;
+	float secondNum;
+	float answer;
+
+public:
+	WesleyClass(int, int);
+	int subract();
+	WesleyClass();
+	~WesleyClass();
+};
+
