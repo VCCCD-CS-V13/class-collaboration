17fbc9acdf58ebfd0b0e52607b4ab37a128e553b
Steve Mulholland <smulholland2@gmail.com>
2018-02-14 14:17:57 -0800

    Subtraction Class

---
+++ ConsoleApplication1/ConsoleApplication1/subtraction.h
@@ -0,0 +1,13 @@
+#pragma once
+class subtraction
+{
+private:
+	int myStatus;
+public:
+	subtraction1();
+	subtraction1(int);
+	~subtraction();
+	void myClassSubtraction(int, int);
+	int myStatusReturn();
+	void myReturn();
+};
