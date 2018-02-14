ecf9425cbb93d9aa794e3c4cd8cab3a03a51e1e9
VC Science Labs <vscilab@vcccd.edu>
2018-02-14 14:47:17 -0800

    Remake triangle

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,12 +9,10 @@ using namespace std;
 
 int main()
 {
-	josesclass1 tri;
-	int a, b;
+	int columns, rows;
 	cout << "Enter number of rows:";
-	cin >> b;
-	tri.maketri(a, b);
-	/*for (int i = 1, k = 0; i <= rows; i++, k = 0)
+	cin >> rows;
+	for (int i = 1, k = 0; i <= rows; i++, k = 0)
 	{
 		for (columns = 1; columns <= rows - i; ++columns)
 		{
@@ -30,7 +28,7 @@ int main()
 		{
 
 		}
-	}*/
+	}
 
     return 0;
 }
