457c1365044a47a45f258423c37797268976bd2f
vscilab <vscilab@VSCI226007>
2018-02-14 14:28:53 -0800

    Class code

---
+++ ConsoleApplication1/ConsoleApplication1/vincent.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "vincent.h"
+
+
+vincent::vincent()
+{
+}
+
+
+vincent::~vincent()
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/vincent.h
@@ -0,0 +1,8 @@
+#pragma once
+class vincent
+{
+public:
+	vincent();
+	~vincent();
+};
+
