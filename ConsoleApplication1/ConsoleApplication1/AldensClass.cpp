316fd0c21bff6b9bc6620790a91a4746fa96720a
Alden Smith <aldensmith06@Gmail.com>
2018-02-14 13:50:00 -0800

    Added words

---
+++ ConsoleApplication1/ConsoleApplication1/AldensClass.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "AldensClass.h"
+
+
+Class::Class()
+{
+}
+
+
+Class::~Class()
+{
+}
---
+++ ConsoleApplication1/ConsoleApplication1/AldensClass.h
@@ -0,0 +1,8 @@
+#pragma once
+class Class
+{
+public:
+	Class();
+	~Class();
+};
+
