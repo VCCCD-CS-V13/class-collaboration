13b6dafb4758234fbaa12a502974711b5973e702
RogerClass <roger.lorelli@yahoo.com>
2018-02-14 13:40:41 -0800

    added some private variables

--- ConsoleApplication1/ConsoleApplication1/Rogers_Class.cpp
+++ ConsoleApplication1/ConsoleApplication1/Rogers_Class.cpp
@@ -4,8 +4,19 @@
 
 Rogers_Class::Rogers_Class()
 {
+	sides = 1;
+	radius = 1
 }
 
+Rogers_Class::Rogers_Class(int a)
+{
+	sides = a;
+}
+
+Rogers_Class::Rogers_Class(int a)
+{
+	radius = a;
+}
 
 Rogers_Class::~Rogers_Class()
 {
--- ConsoleApplication1/ConsoleApplication1/Rogers_Class.h
+++ ConsoleApplication1/ConsoleApplication1/Rogers_Class.h
@@ -1,8 +1,14 @@
 #pragma once
 class Rogers_Class
 {
+private:
+	int sides;
+	int radius;
+
 public:
 	Rogers_Class();
+	Rogers_Class(int sides);
+	Rogers_Class(int radius);
 	virtual ~Rogers_Class();
 };
 
